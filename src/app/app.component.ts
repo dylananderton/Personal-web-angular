727538e2a569605958d9648c7e9d598e77749a6a
Dylan <dylan.anderton@cakap.id>
2023-02-18 23:36:57 +0700

    half-done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,20 @@
 import { Component } from '@angular/core';
-import { Breakpoints } from '@angular/cdk/layout';
 import { OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: []
 })
 export class AppComponent implements OnInit{
 
   title = 'Personal-Website';
 
+  constructor(){}
+
   ngOnInit(): void {
-  console.log('Web ' + Breakpoints.Web);
-  console.log('WebLandscape ' + Breakpoints.WebLandscape);
-  console.log('WebPortrait ' + Breakpoints.WebPortrait);
-
-  console.log('Tablet ' + Breakpoints.Tablet);
-  console.log('TabletPortrait ' + Breakpoints.TabletPortrait);
-  console.log('TabletLandscape ' + Breakpoints.TabletLandscape);
-
-  console.log('Handset ' + Breakpoints.Handset);
-  console.log('HandsetLandscape ' + Breakpoints.HandsetLandscape);
-  console.log('HandsetPortrait ' + Breakpoints.HandsetPortrait);
-
-  console.log('XSmall ' + Breakpoints.XSmall);
-  console.log('Small ' + Breakpoints.Small);
-  console.log('Medium ' + Breakpoints.Medium);
-  console.log('Large ' + Breakpoints.Large);
-  console.log('XLarge ' + Breakpoints.XLarge);
   
   }
   
